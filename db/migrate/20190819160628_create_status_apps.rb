045f1f642320aba48d81008d353ee998ef064780
Caitlin Snyder <caitsnyder@gmail.com>
2019-08-19 15:14:51 -0400

    views for status_apps and projects

--- db/migrate/20190819160628_create_status_apps.rb
+++ db/migrate/20190819160628_create_status_apps.rb
@@ -2,6 +2,7 @@ class CreateStatusApps < ActiveRecord::Migration[5.2]
   def change
     create_table :status_apps do |t|
       t.string :status
+      t.text :request
       t.references :project, foreign_key: true
       t.references :user, foreign_key: true
 
