d96f69a9873460f2d8880e4523352d57d6d29b9c
Khaoula Zghoumi <khaoulaz45@gmail.com>
2019-08-19 17:52:20 -0400

    added value cards

--- config/routes.rb
+++ config/routes.rb
@@ -6,15 +6,15 @@ Rails.application.routes.draw do
 
   root to: 'pages#home'
 
-#   resources :user, only: [:index, :show, :update] do
-#     resources :project, only: [:index, :show]
-#     resources :application, only: [:update, :new, :create]
-#   end
+  resources :user, only: [:index, :show, :update] do
+    resources :project, only: [:index, :show]
+    resources :application, only: [:update, :new, :create]
+  end
 
-#   resources :organization, only: [:index, :show, :update] do
-#     resources :project, only: [:index, :show, :new, :create, :update]
-#     resources :application, only: [:index, :show, :update]
-#   end
+  resources :organization, only: [:index, :show, :update] do
+    resources :project, only: [:index, :show, :new, :create, :update]
+    resources :application, only: [:index, :show, :update]
+  end
 
-#   resources :projects, only: [:index, :show]
+  resources :projects, only: [:index, :show]
 end
