9c8357e3753b1c3d2547165d112e9c562543dd68
Caitlin Snyder <caitsnyder@gmail.com>
2019-08-20 17:01:54 -0400

    homepage-links

--- db/schema.rb
+++ db/schema.rb
@@ -29,11 +29,13 @@ ActiveRecord::Schema.define(version: 2019_08_20_140820) do
   create_table "organizations", force: :cascade do |t|
     t.string "name"
     t.text "mission"
+    t.string "logo"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
   create_table "projects", force: :cascade do |t|
+    t.string "title"
     t.string "description"
     t.bigint "organization_id"
     t.datetime "created_at", null: false
