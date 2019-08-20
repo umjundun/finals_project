d01c6a0ee49c7e277f2de14c59d4cd62485f16fb
Caitlin Snyder <caitsnyder@gmail.com>
2019-08-20 14:25:29 -0400

    user controller commented out

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,22 +1,22 @@
 class UsersController < ApplicationController
-  def index
-    @users = policy_scope(User).order(created_at: :desc)
-  end
+  # def index
+  #   @users = policy_scope(User).order(created_at: :desc)
+  # end
 
-  def show
-    authorize @user
-  end
+  # def show
+  #   authorize @user
+  # end
 
-  def edit
-    authorize @user
-  end
+  # def edit
+  #   authorize @user
+  # end
 
-  def update
-    authorize @user
-    if @user.update(user_params)
-      redirect_to root_path(@user)
-    else
-      render :edit
-    end
-  end
+  # def update
+  #   authorize @user
+  #   if @user.update(user_params)
+  #     redirect_to root_path(@user)
+  #   else
+  #     render :edit
+  #   end
+  # end
 end
