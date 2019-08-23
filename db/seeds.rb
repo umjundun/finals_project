3cfd6fee676a486a786e6b5e645e396e2d52baff
Michael JunDundas <umjundun@gmail.com>
2019-08-23 12:22:22 -0400

    redone seed file to be more workable

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,28 +1,10 @@
-#Default Organization for Dev users
+
+#Default Dummy Organization for Dev users
 Organization.create!(name: 'default', mission: 'none')
 
-# Organizations
-4.times do
-  name = Faker::Company.name
-  mission = Faker::Company.catch_phrase
-  logo = Faker::Company.logo
-  address = Faker::Address.full_address
-  lat = Faker::Address.latitude
-  long = Faker::Address.longitude
-  Organization.create(name: name, mission: mission, latitude: lat, longitude: long, logo: logo, address: address)
-end
-p "4 organizations created"
-
-# 2 Projects per organization
-2.times do
-  Organization.all.each do |org|
-    organization = org
-    title = Faker::Job.title
-    description = Faker::Movies::HitchhikersGuideToTheGalaxy.quote
-    Project.create(organization: organization, description: description, title: title)
-  end
-end
-# Organization.create(name: 'Valve', mission: "make money", logo: Faker::Company.logo)
+# Organizations for users associated with an organization
+Organization.create(name: 'Valve', mission: 'To help the hungry people!', address: "561 Bannerman Ave, Winnipeg", logo: Faker::Company.logo)
+Organization.create(name: 'Ubisoft', mission: 'To provide electricity to people!', address: '5505 Boul St-Laurent #2000, Montreal', logo: Faker::Company.logo)
 p "2 organizations created"
 
 # Users - Admin
@@ -33,20 +15,9 @@ first_name = "Admin"
 last_name = "Test"
 admin = true
 developer = false
-new_admin = User.create!(organization: Organization.find(2), email: email, username: username, password: password, first_name: first_name, last_name: last_name, admin: admin, developer: developer)
+new_admin = User.create!(organization: Organization.find(1), email: email, username: username, password: password, first_name: first_name, last_name: last_name, admin: admin, developer: developer)
 p "1 admin user created"
 # new_admin = User.new(organization_id: Organization.all.sample, email: email, username: username, password: password, first_name: first_name, last_name: last_name, admin: admin, developer: developer)
-
-# Users - Dev
-email = "dev@test.com"
-username = "developer"
-password = "password"
-first_name = "Dev"
-last_name = "Test"
-admin = false
-developer = true
-new_dev = User.create!(organization: Organization.find(2), email: email, username: username, password: password, first_name: first_name, last_name: last_name, admin: admin, developer: developer)
-p "1 developer user created"
 
 # Users - NGO
 email = "ngo@test.com"
@@ -60,21 +31,17 @@ new_rep = User.create!(organization: Organization.find(2), email: email, usernam
 p "1 ngo_rep user created"
 
 # 2 Projects per organization
-2.times do
-  Organization.all.each do |org|
-    organization = org
-    title = Faker::Job.title
-    description = Faker::Movies::HitchhikersGuideToTheGalaxy.quote
-    Project.create!(organization: organization, description: description, title: title, user: new_rep)
-  end
-end
-p "2 projects created"
+Project.create(organization: Organization.find(2), description: Faker::Movies::HitchhikersGuideToTheGalaxy.quote, title: Faker::Job.title, user: new_rep)
+Project.create(organization: Organization.find(2), description: Faker::Movies::HitchhikersGuideToTheGalaxy.quote, title: Faker::Job.title, user: new_rep)
+Project.create(organization: Organization.find(2), description: Faker::Movies::HitchhikersGuideToTheGalaxy.quote, title: Faker::Job.title, user: new_rep)
+# Organization.create(name: 'Valve', mission: "make money", logo: Faker::Company.logo)
+
 Project.all.each do |project|
   p project.title
 end
 
 # Set staff membership
-test_ngo = Organization.find(4)
+test_ngo = Organization.find(2)
 p "test_org created: #{test_ngo}"
 test_rep = Representative.create(organization: test_ngo, user: new_rep)
 p "test_rep created: #{test_rep}"
