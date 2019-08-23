9eca68fd2ed9ceb70e100885b20b5db957d3fbcf
Michael JunDundas <umjundun@gmail.com>
2019-08-23 14:22:41 -0400

    more fixes

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,7 +4,7 @@ Organization.create!(name: 'default', mission: 'none')
 
 # Organizations for users associated with an organization
 Organization.create(name: 'Valve', mission: 'To help the hungry people!', address: "561 Bannerman Ave, Winnipeg", logo: Faker::Company.logo)
-Organization.create(name: 'Ubisoft', mission: 'To provide electricity to people!', address: '5505 Boul St-Laurent #2000, Montreal', logo: Faker::Company.logo)
+Organization.create(name: 'Ubisoft', mission: 'To provide electricity to people!', address: '5505 St Laurent Blvd, Montreal', logo: Faker::Company.logo)
 p "2 organizations created"
 
 # Users - Admin
