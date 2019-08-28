abcba67d79307d3010404395f2dca9db5cbc3f98
Khaoula Zghoumi <khaoulaz45@gmail.com>
2019-08-28 14:04:29 -0400

    XXXX

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_27_142207) do
+ActiveRecord::Schema.define(version: 2019_08_26_161225) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,7 +23,7 @@ ActiveRecord::Schema.define(version: 2019_08_27_142207) do
   end
 
   create_table "engagements", force: :cascade do |t|
-    t.string "status", default: "developer"
+    t.string "status"
     t.text "request"
     t.bigint "project_id"
     t.bigint "user_id"
@@ -70,15 +70,14 @@ ActiveRecord::Schema.define(version: 2019_08_27_142207) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "user_id"
-    t.boolean "active", default: true
     t.index ["organization_id"], name: "index_projects_on_organization_id"
     t.index ["user_id"], name: "index_projects_on_user_id"
   end
 
   create_table "representatives", force: :cascade do |t|
-    t.boolean "primary"
     t.bigint "user_id"
     t.bigint "organization_id"
+    t.boolean "primary"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["organization_id"], name: "index_representatives_on_organization_id"
@@ -102,18 +101,17 @@ ActiveRecord::Schema.define(version: 2019_08_27_142207) do
     t.string "linkedin"
     t.string "bio"
     t.string "github"
-    t.string "job_title"
+    t.boolean "admin", default: false
+    t.boolean "developer", default: true
     t.string "photo"
     t.boolean "hibernate"
-    t.boolean "developer", default: true
-    t.boolean "admin", default: false
+    t.bigint "organization_id"
+    t.string "job_title"
     t.string "contact_number"
     t.string "contact_email"
     t.string "interests"
     t.string "experience"
     t.string "skills"
-    t.string "group", default: "developer"
-    t.bigint "organization_id"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["organization_id"], name: "index_users_on_organization_id"
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
