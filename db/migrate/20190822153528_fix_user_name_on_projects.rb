56892e77aa2f74c8eacf00d64e144aa189c61983
Caitlin Snyder <catisnyder@gmail.com>
2019-08-28 10:33:58 -0400

    Debugged and consolidated migrations

--- db/migrate/20190822153528_fix_user_name_on_projects.rb
+++
@@ -1,6 +0,0 @@
-class FixUserNameOnProjects < ActiveRecord::Migration[5.2]
-  def change
-    remove_reference :projects, :users
-    add_reference :projects, :user, index: true, foreign_key: true
-  end
-end
--- db/migrate/20190826160046_add_group_to_user.rb
+++
@@ -1,5 +0,0 @@
-class AddGroupToUser < ActiveRecord::Migration[5.2]
-  def change
-    add_column :users, :group, :string, inclusion: { in: ["developer", "ngo", "admin"] }, :default => "developer"
-  end
-end
