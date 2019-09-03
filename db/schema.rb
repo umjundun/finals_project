b5caf990392d6323c0b75e6d566553def1d8b764
Samuel Michaud-Morin <samuelmmorin.samimo@gmail.com>
2019-09-03 14:15:04 -0400

    working on ngo dashboard

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_30_163057) do
+ActiveRecord::Schema.define(version: 2019_09_03_154352) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -30,7 +30,7 @@ ActiveRecord::Schema.define(version: 2019_08_30_163057) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "ngo_status", default: ""
-    t.string "dev_status", default: ""
+    t.string "dev_status", default: "Pending dev action"
     t.datetime "start_date"
     t.datetime "end_date"
     t.index ["project_id"], name: "index_engagements_on_project_id"
@@ -62,11 +62,11 @@ ActiveRecord::Schema.define(version: 2019_08_30_163057) do
     t.string "name"
     t.text "mission"
     t.string "logo"
+    t.float "longitude"
+    t.float "latitude"
+    t.string "address"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "address"
-    t.float "latitude"
-    t.float "longitude"
   end
 
   create_table "pg_search_documents", force: :cascade do |t|
@@ -131,15 +131,15 @@ ActiveRecord::Schema.define(version: 2019_08_30_163057) do
     t.string "bio"
     t.string "github"
     t.string "job_title"
-    t.string "photo"
-    t.boolean "hibernate"
-    t.boolean "developer", default: true
-    t.boolean "admin", default: false
     t.string "contact_number"
     t.string "contact_email"
     t.string "interests"
     t.string "experience"
     t.string "skills"
+    t.string "photo"
+    t.boolean "hibernate"
+    t.boolean "admin", default: false
+    t.boolean "developer", default: true
     t.string "group", default: "developer"
     t.bigint "organization_id"
     t.index ["email"], name: "index_users_on_email", unique: true
